391368e103485d25c130f9d702772290d2053c42
David Tolnay <dtolnay@gmail.com>
2017-12-17 11:37:18 -0800

    Release 0.10.3

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,7 @@
 //! # fn main() {}
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/serde_bytes/0.10.2")]
+#![doc(html_root_url = "https://docs.rs/serde_bytes/0.10.3")]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(feature = "alloc", feature(alloc))]
 #![deny(missing_docs)]
