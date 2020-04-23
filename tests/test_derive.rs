21928b3d0a713b8fe045ec4a2e24471159ae40d8
David Tolnay <dtolnay@gmail.com>
2020-04-22 21:53:16 -0700

    Test tokens of struct with derive

--- tests/test_derive.rs
+++ tests/test_derive.rs
@@ -1,5 +1,6 @@
 use serde_bytes::{ByteBuf, Bytes};
 use serde_derive::{Deserialize, Serialize};
+use serde_test::{assert_tokens, Token};
 use std::borrow::Cow;
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
@@ -33,4 +34,45 @@ struct Test<'a> {
 struct Dst {
     #[serde(with = "serde_bytes")]
     bytes: [u8],
+}
+
+#[test]
+fn test() {
+    let test = Test {
+        slice: b"...",
+        vec: b"...".to_vec(),
+        bytes: Bytes::new(b"..."),
+        byte_buf: ByteBuf::from(b"...".as_ref()),
+        cow_slice: Cow::Borrowed(b"..."),
+        cow_bytes: Cow::Borrowed(Bytes::new(b"...")),
+        boxed_slice: b"...".to_vec().into_boxed_slice(),
+        boxed_bytes: ByteBuf::from(b"...".as_ref()).into_boxed_bytes(),
+    };
+
+    assert_tokens(
+        &test,
+        &[
+            Token::Struct {
+                name: "Test",
+                len: 8,
+            },
+            Token::Str("slice"),
+            Token::BorrowedBytes(b"..."),
+            Token::Str("vec"),
+            Token::Bytes(b"..."),
+            Token::Str("bytes"),
+            Token::BorrowedBytes(b"..."),
+            Token::Str("byte_buf"),
+            Token::Bytes(b"..."),
+            Token::Str("cow_slice"),
+            Token::BorrowedBytes(b"..."),
+            Token::Str("cow_bytes"),
+            Token::BorrowedBytes(b"..."),
+            Token::Str("boxed_slice"),
+            Token::Bytes(b"..."),
+            Token::Str("boxed_bytes"),
+            Token::Bytes(b"..."),
+            Token::StructEnd,
+        ],
+    );
 }
