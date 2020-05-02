2a07ef7c4809e475d0cbedc2e0222dbb737ca873
David Tolnay <dtolnay@gmail.com>
2020-05-01 21:53:04 -0700

    Release 0.11.4

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/serde_bytes/0.11.3")]
+#![doc(html_root_url = "https://docs.rs/serde_bytes/0.11.4")]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![deny(missing_docs)]
 
