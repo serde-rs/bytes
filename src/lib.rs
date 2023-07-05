5e6b0762115ac322834d7ac468ecd510f0eb2045
David Tolnay <dtolnay@gmail.com>
2023-07-05 09:59:45 -0700

    Release 0.11.11

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/serde_bytes/0.11.10")]
+#![doc(html_root_url = "https://docs.rs/serde_bytes/0.11.11")]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![deny(missing_docs)]
 #![allow(
